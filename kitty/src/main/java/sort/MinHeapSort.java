d5a833ff8616c6e7bf28bf575926ab6963c1c430
joylink_zhangyanpei <yanpei.zhang@joelian.cn>
2019-04-17 17:48:22 +0800

    sort algorithm summary

--- kitty/src/main/java/sort/MinHeapSort.java
+++ kitty/src/main/java/sort/MinHeapSort.java
@@ -4,34 +4,35 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-public class HeapSort extends IArraySort {
+public class MinHeapSort extends IArraySort {
     @Override
     int[] sort(int[] sourceArray) {
         int n = sourceArray.length;
         int[] arr = Arrays.copyOf(sourceArray, n);
-        buildMaxHeap(arr, n);
+        buildMinHeap(arr, n);
         for (int i = n - 1; i > 0; i--) {
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
+            int temp = arr[i];
+            arr[i] = arr[0];
+            arr[0] = temp;
             heapify(arr, 0, i);
         }
+
         return arr;
     }
 
-    private void buildMaxHeap(int[] arr, int len) {
-        for (int i = (len - 2) / 2; i >= 0; i--) {
-            heapify(arr, i, len);
+    private void buildMinHeap(int[] arr, int n) {
+        for (int i = (n - 1 - 1) / 2; i >= 0; i--) {
+            heapify(arr, i, n);
         }
     }
 
-    private void heapify(int[] arr, int p, int len) {
+    private void heapify(int[] arr, int p, int n) {
         int temp = arr[p];
-        for (int i = 2 * p + 1; i < len; i = 2 * i + 1) {
-            if (i + 1 < len && arr[i+1] > arr[i]) {
+        for (int i = 2 * p + 1; i < n; i = 2 * i + 1) {
+            if (i + 1 < n && arr[i] > arr[i+1]) {
                 i++;
             }
-            if (arr[i] > temp) {
+            if (arr[i] < temp) {
                 arr[p] = arr[i];
                 p = i;
             }
